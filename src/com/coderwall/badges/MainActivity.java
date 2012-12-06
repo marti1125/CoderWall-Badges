cfd3193d6081cef0b842fca2194f4c06ec1908c1
Willy Aguirre <marti1125@gmail.com>
2012-12-06 00:21:07 -0500

    update project package

--- src/com/coderwall/badges/MainActivity.java
+++ src/com/coderwall/badges/MainActivity.java
@@ -3,14 +3,18 @@ package com.coderwall.badges;
 import android.os.Bundle;
 import org.apache.cordova.*;
 
+import com.coderwall.badges.R;
+
+//import com.coderwall.badges.R;
 
 public class MainActivity extends DroidGap {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        super.setIntegerProperty("splashscreen", R.drawable.splash);
-        super.loadUrl("file:///android_asset/www/index.html", 8000);
+    	 super.onCreate(savedInstanceState);
+         super.setIntegerProperty("splashscreen", R.drawable.splash);
+         super.loadUrl("file:///android_asset/www/index.html", 8000);
     }
 
+    
 }
